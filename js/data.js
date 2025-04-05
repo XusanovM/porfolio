58eeed8143888f5ddef0fa3f88fd1342769617b5
Mohirbek <Mohirbek@gmail.com>
2025-04-05 15:17:04 +0500

    Site

---
+++ js/data.js
@@ -0,0 +1,27 @@
+export default [
+  {
+    title: "5-dars",
+    github: "https://github.com/XusanovM/5-dars",
+    vercel: "https://vercel.com/xusanovms-projects/5-dars",
+  },
+  {
+    title: "3-dars",
+    github: "https://github.com/XusanovM/3-dars",
+    vercel: "https://vercel.com/xusanovms-projects/3-dars",
+  },
+  {
+    title: "homework",
+    github: "https://github.com/XusanovM/Homework",
+    vercel: "https://vercel.com/xusanovms-projects/uyga-vzifa3",
+  },
+  {
+    title: "Ansormed",
+    github: "https://github.com/XusanovM/Ansormed",
+    vercel: "https://vercel.com/xusanovms-projects/ansormed",
+  },
+  {
+    title: "homework",
+    github: "https://github.com/XusanovM/web-site",
+    vercel: "https://vercel.com/xusanovms-projects/web-site",
+  },
+];
